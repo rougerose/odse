9e5708d9fa15c2c4ead63c1dc4bf6c92f674dee6
christophe le drean <christophe@rouge-rose.net>
2019-02-27 22:27:56 +0100

    Corrections CSS et JS formulaire recherche

--- theme/javascript/odse.js
+++ theme/javascript/odse.js
@@ -6,12 +6,30 @@ var searchForm = (function() {
     $searchbox = $('.c-search-box');
     $searchInput = $searchbox.find('input.search');
     self.bindActions();
+    self.isfilled($searchInput, '');
   };
 
   self.bindActions = function() {
-    $searchInput.on('focusout', function() {
-      '' !== $(this).val() ? $(this).addClass('is-filled') : $(this).removeClass('is-filled');
+    $searchInput.on('focusout focus', function() {
+      self.isfilled($(this), event);
     });
+  };
+
+  self.isfilled = function($el, e) {
+    $elParent = $el.parent();
+
+    if ('' !== $el.val()) {
+      $el.addClass('is-filled');
+      $elParent.addClass('is-filled');
+    } else {
+      $el.removeClass('is-filled');
+      $elParent.removeClass('is-filled');
+    }
+
+    if (e.type === 'focus' ) {
+      $el.addClass('is-filled');
+      $elParent.addClass('is-filled');
+    }
   };
 
   return self;
