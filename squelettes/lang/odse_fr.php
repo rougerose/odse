9e2931ccd6f7ddeb9e8c6072fcf5536a5167005a
christophe le drean <christophe@rouge-rose.net>
2019-02-27 22:27:56 +0100

    squelette recherche

--- squelettes/lang/odse_fr.php
+++ squelettes/lang/odse_fr.php
@@ -30,7 +30,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
   // R
   'rechercher_titre' => "Rechercher sur le site",
   'rechercher_label' => "Votre recherche",
-
+  'rechercher_resultats_zero_info' => "Aucun résultat",
+  'rechercher_resultat_compte_info' => "1 résultat",
+  'rechercher_resultats_compte_info' => "@nb@ résultats",
+  'rechercher_resultat_articles_compte_info' => "1 article",
+  'rechercher_resultats_articles_compte_info' => "@nb@ articles",
+  'rechercher_resultat_documents_compte_info' => "1 document",
+  'rechercher_resultats_documents_compte_info' => "@nb@ documents",
+  'rechercher_resultat_mots_compte_info' => "1 mot-clé",
+  'rechercher_resultats_mots_compte_info' => "@nb@ mots-clés",
+  'rechercher_resultat_articles_info' => "L’article",
+  'rechercher_resultats_articles_info' => "Les articles",
+  'rechercher_resultat_mots_info' => "Le mot-clé",
+  'rechercher_resultats_mots_info' => "Les mots-clés",
+  'rechercher_resultat_documents_info' => "Le document",
+  'rechercher_resultats_documents_info' => "Les documents",
   // S
   'savoir_plus_info' => "+ En savoir plus",
   'site_nav_titre' => "Menu",
